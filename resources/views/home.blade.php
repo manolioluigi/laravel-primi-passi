4701efaf2f3fd866576a873ec2085b1dc9adcf91
manolioluigi <luigi.manolio1999@gmail.com>
2023-02-15 15:03:36 +0100

    primo push hello world

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+</head>
+<body>
+
+    <h1>Hello World</h1>
+
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1></h1>
+                    <ul>
+                        <li></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+
+</body>
+</html>
